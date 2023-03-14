80d108d8188e26e13bb381070d4dc0def57caf55
unknown <harfiantobinjeman@gmail.com>
2023-03-15 03:00:22 +0700

    crud

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
 
 //panggil control
-import { getProduct } from "../controller/ProductController.js";
+import { getProduct,
+    getAllProduct,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct
+ } from "../controller/ProductController.js";
 const router = express.Router();
 
 router.get('/products',getProduct);
+router.get('/productsAll',getAllProduct);
+router.get('/products/:id',getProductById);
+router.post('/products',createProduct);
+router.patch('/products/:id',updateProduct);
+router.delete('/products/:id',deleteProduct);
 
 export default router;
